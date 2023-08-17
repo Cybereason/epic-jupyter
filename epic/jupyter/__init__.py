e859778389f650dc8a7efdc47086f8319ddd3808
Yonatan Perry <perry@cybereason.com>
2023-08-17 09:46:32 -0400

    interpolate: fix an issue where importing epic.jupyter would fail when IPython is not installed
    - do not auto-import the 'interpolate' module when importing the package

--- epic/jupyter/__init__.py
+++ epic/jupyter/__init__.py
@@ -2,7 +2,6 @@ from .ipython import IpythonNotAvailable, get_ipython, is_ipython, is_jupyter_no
 from .display import display_if_ipython, markdown, side_by_side
 from .initialization import event_register_once
 from .nbcode import use_notebook_code
-from .interpolate import load_ipython_extension as _load1
 
 
 def load_ipython_extension(ipython):
@@ -12,4 +11,5 @@ def load_ipython_extension(ipython):
     Do not call this function directly.
     Instead, use `%loadext epic.jupyter` or add it to IPython configuration.
     """
-    _load1(ipython)
+    from .interpolate import load_ipython_extension as load_interpolate_extension
+    load_interpolate_extension(ipython)
