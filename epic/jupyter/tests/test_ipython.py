85f3313a6148aa4eb7972ee4297d1cf8506f1b0e
Yonatan Perry <perry@cybereason.com>
2023-08-17 10:07:10 -0400

    tests: check that a general import of the package and its ipython-detection mechanisms work when IPython is not installed

--- epic/jupyter/tests/test_ipython.py
+++ epic/jupyter/tests/test_ipython.py
@@ -1,12 +1,11 @@
+import sys
 import pytest
-
-from epic.jupyter import get_ipython, IpythonNotAvailable
-
-from .helpers import ipython_shell
 
 
 class TestIpython:
     def test_get_ipython(self, tmp_path):
+        from epic.jupyter import get_ipython, IpythonNotAvailable
+        from .helpers import ipython_shell
         assert get_ipython() is None
         assert get_ipython(strict=False) is None
         with pytest.raises(IpythonNotAvailable):
@@ -15,3 +14,19 @@ class TestIpython:
             assert get_ipython() is ipython
             assert get_ipython(strict=False) is ipython
             assert get_ipython(strict=True) is ipython
+
+    def test_no_ipython_installed(self, tmp_path):
+        mod = sys.modules['IPython']
+        sys.modules['IPython'] = None
+        for k in list(sys.modules):
+            if k.startswith("epic.jupyter"):
+                sys.modules.pop(k)
+        assert 'epic.jupyter' not in sys.modules
+        try:
+            from epic.jupyter import get_ipython, IpythonNotAvailable
+            assert get_ipython() is None
+            assert get_ipython(strict=False) is None
+            with pytest.raises(IpythonNotAvailable):
+                get_ipython(strict=True)
+        finally:
+            sys.modules['IPython'] = mod
